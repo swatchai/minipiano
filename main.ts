f915e06d53098ce5ca05c580e42a53da668d2bd6
swatchai <fsvskkeng@yahoo.com>
2024-11-07 18:12:58 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,170 @@
+function Moctave () {
+    if (key == piano.TouchButton(piano.touch.C)) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.CD)) {
+        music.play(music.tonePlayable(277, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.D)) {
+        music.play(music.tonePlayable(294, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.DE)) {
+        music.play(music.tonePlayable(311, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.E)) {
+        music.play(music.tonePlayable(330, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.F)) {
+        music.play(music.tonePlayable(349, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.FG)) {
+        music.play(music.tonePlayable(370, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.G)) {
+        music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.GA)) {
+        music.play(music.tonePlayable(415, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.A)) {
+        music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.AB)) {
+        music.play(music.tonePlayable(466, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.B)) {
+        music.play(music.tonePlayable(494, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+}
+function Moctave2 () {
+    if (key == piano.TouchButton(piano.touch.C)) {
+        music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.CD)) {
+        music.play(music.tonePlayable(554, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.D)) {
+        music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.DE)) {
+        music.play(music.tonePlayable(622, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.E)) {
+        music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.F)) {
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.FG)) {
+        music.play(music.tonePlayable(740, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.G)) {
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.GA)) {
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.A)) {
+        music.play(music.tonePlayable(880, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.AB)) {
+        music.play(music.tonePlayable(932, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.B)) {
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+}
+function Moctave0 () {
+    if (key == piano.TouchButton(piano.touch.C)) {
+        music.play(music.tonePlayable(131, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.CD)) {
+        music.play(music.tonePlayable(139, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.D)) {
+        music.play(music.tonePlayable(147, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.DE)) {
+        music.play(music.tonePlayable(156, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.E)) {
+        music.play(music.tonePlayable(165, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.F)) {
+        music.play(music.tonePlayable(175, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.FG)) {
+        music.play(music.tonePlayable(185, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.G)) {
+        music.play(music.tonePlayable(196, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.GA)) {
+        music.play(music.tonePlayable(208, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.A)) {
+        music.play(music.tonePlayable(220, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.AB)) {
+        music.play(music.tonePlayable(233, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    if (key == piano.TouchButton(piano.touch.B)) {
+        music.play(music.tonePlayable(247, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+}
+let key = 0
+piano.RGB_Program_Close()
+music.setVolume(127)
+let octave = 1
+led.setBrightness(70)
+basic.showLeds(`
+    # . . . #
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+music.setBuiltInSpeakerEnabled(false)
+basic.forever(function () {
+    key = piano.Touch()
+    if (key == piano.TouchButton(piano.touch.L)) {
+        octave = 0
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # # # # #
+            `)
+    }
+    if (key == piano.TouchButton(piano.touch.M)) {
+        octave = 1
+        basic.showLeds(`
+            # . . . #
+            # # . # #
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (key == piano.TouchButton(piano.touch.H)) {
+        octave = 2
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # # # # #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (octave == 0) {
+        Moctave0()
+    }
+    if (octave == 1) {
+        Moctave()
+    }
+    if (octave == 2) {
+        Moctave2()
+    }
+})
